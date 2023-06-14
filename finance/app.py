6dd938ebe217fb13cf588b1202eddd9959d385ee
alladishashank <shashankalladi@MacBook-Pro-2.local>
2023-06-14 14:50:40 +0530

    002: added authentication

--- finance/app.py
+++ finance/app.py
@@ -24,6 +24,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# authentication added
+
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
