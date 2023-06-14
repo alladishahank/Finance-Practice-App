ed96646472b36ebec845204d47ea46995bd438d2
alladishashank <shashankalladi@MacBook-Pro-2.local>
2023-06-14 14:40:15 +0530

    001: Added registration

--- finance/app.py
+++ finance/app.py
@@ -24,6 +24,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Branch test
+
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
